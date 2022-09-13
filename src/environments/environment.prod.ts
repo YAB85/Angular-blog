5085a8fc76dc3c662721597358a7ac043cdf2bf9
Yaroslav Bova <46503563+YAB85@users.noreply.github.com>
2022-09-13 14:55:23 +0300

    Delete environment.prod.ts

--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-import { Environment } from "./interface";
-
-export const environment: Environment = {
-  production: true,
-  apiKey: 'AIzaSyB8hUkRv-HI_tujXlnNI8BIuOgYJyjguPA',
-  fbDbUrl: 'https://angular-blog-3c980-default-rtdb.europe-west1.firebasedatabase.app'
-};
